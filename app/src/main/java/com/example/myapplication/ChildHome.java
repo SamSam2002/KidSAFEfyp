0c16b7fe5c0832981ebd8f6f645f70b8d68ba872
SamSam2002 <104634393+SamSam2002@users.noreply.github.com>
2023-02-07 19:07:43 +0545

    Home childpage UI

---
+++ app/src/main/java/com/example/myapplication/ChildHome.java
@@ -0,0 +1,27 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.content.Intent;
+import android.graphics.Color;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.GridLayout;
+import android.widget.GridView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+
+public class ChildHome extends AppCompatActivity {
+    GridLayout mainGrid;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child_home);
+
+    }
+}
