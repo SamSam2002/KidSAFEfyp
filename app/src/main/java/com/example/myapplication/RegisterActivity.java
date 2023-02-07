dd409f25d9cd8cff4ad0c2e06989cf96b07c61ef
SamSam2002 <104634393+SamSam2002@users.noreply.github.com>
2023-02-07 09:08:53 +0545

    Backend register

--- app/src/main/java/com/example/myapplication/RegisterActivity.java
+++ app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -1,16 +1,93 @@
 package com.example.myapplication;
 
+import static android.app.ProgressDialog.show;
+
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import cn.pedant.SweetAlert.SweetAlertDialog;
 
 public class RegisterActivity extends AppCompatActivity {
-    
+
+    EditText editTextEmail, editTextPassword, editTextName, editTextPhone;
+    Button register;
+    Button back_to_login;
+    password_db myDatabase;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        myDatabase = new password_db(this);
+        editTextEmail = findViewById(R.id.email);
+        editTextPassword = findViewById(R.id.password);
+        editTextName = findViewById(R.id.PersonName);
+        editTextPhone = findViewById(R.id.phone);
+        Register_User();
+        back_to_login = findViewById(R.id.back_to_login);
+        back_to_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                final Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                startActivity(i);
+            }
+
+        });
+
+
+    }
+
+    private void Register_User() {
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String Name = editTextName.getText().toString();
+                String Phone = editTextName.getText().toString();
+                String email = editTextEmail.getText().toString();
+                String password = editTextPassword.getText().toString();
+                if (!isEmailValid(email)) {
+                    Toast.makeText(RegisterActivity.this, "Not a valid email", Toast.LENGTH_SHORT).show();
+                } else if (!isPasswordValid(password)) {
+                    Toast.makeText(RegisterActivity.this, "Too short password", Toast.LENGTH_SHORT).show();
+                } else if (email.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Fill all the empty fields", Toast.LENGTH_SHORT).show();
+
+                } else if (password.isEmpty()) {
+                    Toast.makeText(RegisterActivity.this, "Not a valid email", Toast.LENGTH_SHORT).show();
+                } else {
+                    boolean isInserted = myDatabase.insertData(email, password);
+                    new SweetAlertDialog(RegisterActivity.this, SweetAlertDialog.SUCCESS_TYPE)
+                            .setTitleText("Message")
+                            .setContentText("You are Registered")
+                            .setConfirmText("OK")
+                            .setConfirmClickListener(new SweetAlertDialog.OnSweetClickListener() {
+                                @Override
+                                public void onClick(SweetAlertDialog sweetAlertDialog) {
+                                    Intent i = new Intent(RegisterActivity.this, LoginActivity.class);
+                                    startActivity(i);
+                                }
+                            })
+                            .show();
+
+
+                }
+            }
+
+
+            private boolean isEmailValid(String email) {
+                return email.contains("@");
+            }
+
+            private boolean isPasswordValid(String password) {
+                return password.length() > 5;
+            }
+        });
     }
 }
-
